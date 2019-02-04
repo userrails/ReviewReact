d1f2335ed3bd020f7db4577340985da8b33fbd35
Shiv Raj Badu <shivrajbadu@gmail.com>
2019-02-04 17:05:10 +0545

    PrivateRoute component accepts components, import fakeAuth component and if Authenticated then render component else redirect to login page

---
+++ src/hoc/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { fakeAuth } from './../services/fakeAuth'
+
+export function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        fakeAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
